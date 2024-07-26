08b8d176a1e470e9c29e5936a5b738fe9c8c4c4d
ansk <adjevinsk@gmail.com>
2024-07-26 15:48:44 +0200

    Modification du microdata

--- assets/gallery.js
+++ assets/gallery.js
@@ -269,3 +269,18 @@ next =
   };
 })(jQuery);
 
+$(document).ready(function() {
+  $('.gallery').mauGallery({
+      columns: {
+          xs: 1,
+          sm: 2,
+          md: 3,
+          lg: 3,
+          xl: 3
+      },
+      lightBox: true,
+      lightboxId: 'myAwesomeLightbox',
+      showTags: true,
+      tagsPosition: 'top'
+  });
+});
